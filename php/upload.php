c84549578ff6dc7020d1fe340ab62dc6fd1c2648
Heriberto Juárez <heribertojuarezj@gmail.com>
2019-06-26 04:03:45 -0500

    Now the upload's directory is automatically created if it doesn't exist.

--- php/upload.php
+++ php/upload.php
@@ -1,5 +1,13 @@
 <?php
 
+
+/*
+ * Create upload directory if it does not exist
+ **/
+if (!file_exists('../upload')) {
+    mkdir('../upload', 0777, true); //0777 is the octal representation of the rwx Read Write Execute file's permissions
+}
+
 $images = $_FILES['img'];
 for($i = 0; $i<count($images['name']); $i++){
     move_uploaded_file($images['tmp_name'][$i], '../upload/' . $images['name'][$i]);
